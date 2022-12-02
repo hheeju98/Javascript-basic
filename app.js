d8aa523e7e7b8690e3f601f8a83e7f9361cf8b73
hheeju98 <hheeju98@naver.com>
2022-12-02 23:48:38 +0900

    array 개념

---
+++ app.js
@@ -0,0 +1,13 @@
+
+const daysOfWeek = ["mon" , "tue" , "wed" , "thu" ,"fri" , "sat"];
+
+// Get Item from Array
+console.log(daysOfWeek);
+
+// Add one more date to the array
+daysOfWeek.push("sun");
+
+console.log(daysOfWeek);
+
+
+
