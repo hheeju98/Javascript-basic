651f5bd5302881281ed0ae2cb78860c72558ee6e
hheeju98 <hheeju98@naver.com>
2022-12-04 00:08:43 +0900

    Conditionals part Three

--- app.js
+++ app.js
@@ -2,10 +2,13 @@
 
 // console.log(); // false -> age is number라는 의미
 const age = parseInt(prompt("How old are you?"));
-if(isNaN(age)){
- // condition == true
- console.log("Please write a number");
+
+if (isNaN(age) || age < 0){
+    console.log("Please write a number");
+}else if (age < 18){
+console.log("Your are too young.");
+}else if(age >= 18 && age <= 50){
+    console.log("You can drink");
 }else{
-// condition == false
-console.log("Thank you for writing your age.");
+console.log("You can't drink");
 }
