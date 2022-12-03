61b357de95e1754f40e9a90f6e4cf2dd75572b25
hheeju98 <hheeju98@naver.com>
2022-12-03 18:13:06 +0900

    conditionals part Two

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
-const age = prompt("How old are you?"); // 자바 스크립트 일시정지
+//const age = NaN; // 자바 스크립트 일시정지
 
-console.log(typeof "15", typeof parseInt("15")); // parseInt String-> Int로 숫자가 아닌걸 넣으면 NaN으로 나옴
+// console.log(); // false -> age is number라는 의미
+const age = parseInt(prompt("How old are you?"));
+if(isNaN(age)){
+ // condition == true
+ console.log("Please write a number");
+}else{
+// condition == false
+console.log("Thank you for writing your age.");
+}
