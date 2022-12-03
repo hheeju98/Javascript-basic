1050fcae8bf1c9adaed3886163eece03d72cf11a
hheeju98 <hheeju98@naver.com>
2022-12-03 15:26:30 +0900

    functions

--- app.js
+++ app.js
@@ -1,19 +1,29 @@
+function sayHello(nameOfPerson, age){
+    console.log("Hello my name is "+ nameOfPerson + " and I'm " + age);
+}
+
+sayHello("nico", 10);
+sayHello("dal", 23);
+
+function plus(a, b){
+    console.log(a + b);
+}
+
+function divide(a,b){
+    console.log(a / b);
+}
+// a , b 는 function박스 안에서만 존재한다. console.log(a); 불가
+plus(8, 60);
+divide(98, 2);
+
 const player = {
-    name: "nico",
-    points: 10,
-    fat: true,
+name: "nico",
+sayHello: function(OtherPersonsName){
+console.log("hello! " + OtherPersonsName+ " nice to meet you");
+    },
 };
-
-console.log(player);
-
-player.fat = false;
-// const 안의 무언가는 업데이트 가능, 전체는 불가능
-player.lastName = "patato";
-player.points = player.points + 15;
-console.log(player);
-//console.log(player.name);
-//console.log(player["name"]);
-// console = object
-
+console.log(player.name);
+player.sayHello("lynn");
+player.sayHello("nico");
 
 
