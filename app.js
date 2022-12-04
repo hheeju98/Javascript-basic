c1a70b6ad789c0ab8ee833be3fe5b5069182b548
hheeju98 <hheeju98@naver.com>
2022-12-04 22:56:49 +0900

    CSS in Javascript

--- app.js
+++ app.js
@@ -1,40 +1,24 @@
 const h1 = document.querySelector("div.hello:first-child h1"); // querySelector 조건에 맞는 첫번째 값만 가져옴 (querySelectorAll 다가져옴)
 
 function handleTitleClick(){
-    h1.style.color = "blue";
+    const currentColor = h1.style.color;
+    let newColor;
+if (currentColor === "blue"){
+   newColor = "tomato";
+}else{
+    newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
 function handleMouseEnter(){
 h1.innerText = "Mouse is here!";
 }
 
-function handleMouseLeave(){
-h1.innerText = "Mouse is gone!";
-}
 
-function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no Wifi");
-}
-
-function handleWindowOnline(){
-    alert("ALL GOOD");
-}
 
 // h1.onclick = handleTitleClick;
 // h1.onmouseenter = handleMouseEnter;
 h1.addEventListener("click", handleTitleClick);
-h1.addEventListener("mouseenter", handleMouseEnter);
-h1.addEventListener("mouseleave", handleMouseLeave);
 
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offline", handleWindowOffline);
-window.addEventListener("online", handleWindowOnline);
+
