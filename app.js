b11d6a910a627416400724591221e001762d427d
hheeju98 <hheeju98@naver.com>
2022-12-04 23:33:55 +0900

    CSS in Javascript part Two

--- app.js
+++ app.js
@@ -1,24 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1"); // querySelector 조건에 맞는 첫번째 값만 가져옴 (querySelectorAll 다가져옴)
 
 function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-if (currentColor === "blue"){
-   newColor = "tomato";
-}else{
-    newColor = "blue";
+    const clickedClass = "clicked";
+    if (h1.className === clickedClass){
+        h1.className = "";
+    }else{
+        h1.className = clickedClass;
     }
-    h1.style.color = newColor;
 }
 
-function handleMouseEnter(){
-h1.innerText = "Mouse is here!";
-}
-
-
-
-// h1.onclick = handleTitleClick;
-// h1.onmouseenter = handleMouseEnter;
 h1.addEventListener("click", handleTitleClick);
 
 
