4e7a2bbbcc7b13af472e1f4ca96598a484c3b339
hheeju98 <hheeju98@naver.com>
2022-12-04 22:21:29 +0900

    Events part Two

--- app.js
+++ app.js
@@ -1,9 +1,20 @@
 const title = document.querySelector(".hello h1"); // querySelector 조건에 맞는 첫번째 값만 가져옴 (querySelectorAll 다가져옴)
 
-
+console.dir(title);
 
 function handleTitleClick(){
-    title.style.color = "blue"
-    console.log("title was clicked");
+    title.style.color = "blue";
 }
+
+function handleMouseEnter(){
+title.innerText = "Mouse is here!";
+}
+
+function handleMouseLeave(){
+title.innerText = "Mouse is gone!";
+}
+
+
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter", handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
