2a88144ab0c037e61495cbd757b853827e594ac2
hheeju98 <hheeju98@naver.com>
2022-12-04 15:13:24 +0900

    HTML 자바스크립트 관계

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-//const age = NaN; // 자바 스크립트 일시정지
+const title = document.getElementById("title");
 
-// console.log(); // false -> age is number라는 의미
-const age = parseInt(prompt("How old are you?"));
+title.innerText = "Got you!"; 
 
-if (isNaN(age) || age < 0){
-    console.log("Please write a number");
-}else if (age < 18){
-console.log("Your are too young.");
-}else if(age >= 18 && age <= 50){
-    console.log("You can drink");
-}else{
-console.log("You can't drink");
-}
+console.log(title.id);
+console.log(title.className);
