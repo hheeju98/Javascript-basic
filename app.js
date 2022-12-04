279fa5a059589371a3bbeec183c74cacca8ba9c3
hheeju98 <hheeju98@naver.com>
2022-12-04 19:27:09 +0900

    Events

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
 const title = document.querySelector(".hello h1"); // querySelector 조건에 맞는 첫번째 값만 가져옴 (querySelectorAll 다가져옴)
 
-console.log(title);
+
+
+function handleTitleClick(){
+    title.style.color = "blue"
+    console.log("title was clicked");
+}
+title.addEventListener("click", handleTitleClick);
