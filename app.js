ca612f4a11ac108bb66fd51740f4c4f7cc73db3f
hheeju98 <hheeju98@naver.com>
2022-12-04 23:57:40 +0900

    CSS in Javascript part Three

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const h1 = document.querySelector("div.hello:first-child h1"); // querySelector 조건에 맞는 첫번째 값만 가져옴 (querySelectorAll 다가져옴)
 
 function handleTitleClick(){
-    const clickedClass = "clicked";
-    if (h1.className === clickedClass){
-        h1.className = "";
-    }else{
-        h1.className = clickedClass;
-    }
+    h1.classList.toggle("clicked");
+    // if (h1.classList .contains(clickedClass)){
+    //     h1.classList.remove(clickedClass);
+    // }else{
+    //     h1.classList.add(clickedClass);
+    // }
 }
 
 h1.addEventListener("click", handleTitleClick);
